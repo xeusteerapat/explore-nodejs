0921fd02714c7c017af34639b4d7015e079b2341
xeusteerapat <xeus085@gmail.com>
2020-10-09 22:47:21 +0700

    [add] common questions about thread pool

--- threads.js
+++ threads.js
@@ -19,3 +19,10 @@ pbkdf2('pwd', 'salt', 100000, 512, 'sha512', () =>
   console.log(`4: Done in ${Date.now() - start}ms`)
 );
 
+// Common thread pool questions
+// 1. Can we use the threadpool for JS code or can only node.js function use it?
+// - We can write custom JS that uses the thread pool.
+// 2. What functions in node.js std library use the threadpool
+// - All 'fs', 'crypto' stuff (depends on OS)
+// How does this threadpool stuff fit into the event loop?
+// - Tasks running in the threadpool are the 'pendingOperations' in our example
